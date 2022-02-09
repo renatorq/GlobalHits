a4d53296e8ecd598494457a2c85144e907a8884f
renatorq <renato_rq_987@hotmail.com>
2022-02-09 14:48:04 -0500

    Update SaleController.java

--- src/main/java/com/globalhits/pregunta3/controller/SaleController.java
+++ src/main/java/com/globalhits/pregunta3/controller/SaleController.java
@@ -1,13 +1,10 @@
 package com.globalhits.pregunta3.controller;
 
-import com.globalhits.pregunta3.service.SaleServiceImp;
+import com.globalhits.pregunta3.entity.Product;
+import com.globalhits.pregunta3.service.ProductServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/Sales/v1")
